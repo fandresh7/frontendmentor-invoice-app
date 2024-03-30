b914a0468de9721249a915c9a75a1a0f467e8f35
Andres Hortua <fandresh7@gmail.com>
2024-03-30 10:49:32 -0500

    custom colors, images and custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['League Spartan', 'system-ui']
+      },
+      colors: {
+        primary: '#7C5DFA',
+        secondary: '#9277FF',
+        dark1: '#1E2139',
+        dark2: '#252945',
+        light1: '#DFE3FA',
+        light2: '#888EB0',
+        light3: '#7E88C3',
+        dark3: '#0C0E16',
+        accent: '#EC5757',
+        accent2: '#9277FF',
+        lightBg: '#F8F8FB',
+        darkBg: '#141625',
+      },
+
+    },
   },
   plugins: [],
 }
