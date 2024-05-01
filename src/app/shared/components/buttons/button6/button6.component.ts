6049466304ad91cde48c6a16acdd2bf83f9230d0
Andres Hortua <fandresh7@gmail.com>
2024-04-30 22:09:15 -0500

    add type to button6 and remove index param from controlInjector pipe

--- src/app/shared/components/buttons/button6/button6.component.ts
+++ src/app/shared/components/buttons/button6/button6.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { plusIconComponent } from '../../icons.component'
 
 @Component({
   selector: 'app-button6',
   standalone: true,
   imports: [plusIconComponent],
-  template: `<button class="mx-auto w-full heading-s flex justify-center items-center gap-2 bg-lightBg text-light3 hover:bg-light1 transition-colors duration-500 rounded-3xl pt-[17px] pb-[15px] px-6">
+  template: `<button
+    [type]="type"
+    class="mx-auto w-full heading-s flex justify-center items-center gap-2 bg-lightBg text-light3 hover:bg-light1 transition-colors duration-500 rounded-3xl pt-[17px] pb-[15px] px-6">
     <div class="w-2 h-2 text-light3 flex justify-center items-center">
       <app-plus-icon></app-plus-icon>
     </div>
     Add New Item
   </button> `
 })
-export class Button6Component {}
+export class Button6Component {
+  @Input() type: 'submit' | 'button' = 'button'
+}
--- src/app/shared/forms/pipes/control-injector.pipe.ts
+++ src/app/shared/forms/pipes/control-injector.pipe.ts
@@ -10,13 +10,13 @@ import { FormGroup } from '@angular/forms'
 export class ControlInjector implements PipeTransform {
   injector = inject(Injector)
 
-  transform(name: string, control: Control, formGroup?: FormGroup, index?: number): Injector {
+  transform(name: string, control: Control, formGroup?: FormGroup): Injector {
     return Injector.create({
       parent: this.injector,
       providers: [
         {
           provide: CONTROL_DATA,
-          useValue: { key: name, control, formGroup, index }
+          useValue: { key: name, control, formGroup }
         }
       ]
     })
