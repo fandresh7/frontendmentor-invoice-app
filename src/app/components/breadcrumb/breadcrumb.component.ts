b9a9c420c63402dd560187649e966c1d0ef980ad
Andres Hortua <fandresh7@gmail.com>
2024-05-09 15:45:45 -0600

    hover on invoice card, link on each one and link on breadcrumb

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { ArrowLeftIconComponent } from '../../shared/components/icons.component'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [ArrowLeftIconComponent],
+  imports: [RouterLink, ArrowLeftIconComponent],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css'
 })
-export class BreadcrumbComponent {}
+export class BreadcrumbComponent {
+  @Input({ required: true }) link!: unknown[]
+}
--- src/app/components/invoice-card/invoice-card.component.ts
+++ src/app/components/invoice-card/invoice-card.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { CurrencyPipe } from '@angular/common'
+
 import { InvoceStatusComponent } from '../invoce-status/invoce-status.component'
 import { ArrowRightIconComponent } from '../../shared/components/icons.component'
-import { Invoice } from '../../models/invoice'
+
 import { DatesFormatPipe } from '../../pipes/dates-format.pipe'
-import { CurrencyPipe } from '@angular/common'
+import { Invoice } from '../../models/invoice'
 
 @Component({
   selector: 'app-invoice-card',
   standalone: true,
-  imports: [InvoceStatusComponent, ArrowRightIconComponent, DatesFormatPipe, CurrencyPipe],
+  imports: [InvoceStatusComponent, ArrowRightIconComponent, DatesFormatPipe, CurrencyPipe, RouterLink],
   templateUrl: './invoice-card.component.html',
   styleUrl: './invoice-card.component.css'
 })
