1d7ffd79351d7ca2d846770b4f070200dadc94e6
Andres Hortua <fandresh7@gmail.com>
2024-05-20 15:30:39 -0500

    add internationalization configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,14 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  translateService = inject(TranslateService)
+
+  constructor() {
+    const lang = navigator.language || 'en'
+    const code = lang.split('-')[0]
+
+    this.translateService.setDefaultLang(code)
+    this.translateService.use(code)
+  }
+}
